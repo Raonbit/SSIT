b426b161a0a28439b118abad523ac812f236ba05
Raonbit <54011803+Raonbit@users.noreply.github.com>
2019-11-28 13:56:23 +0900

    .

---
+++ notebook/trainer/mprun_demo.py
@@ -0,0 +1,10 @@
+
+# mprun_demo.py 에 함수 저장
+
+def sum_of_lists(N):
+    total = 0
+    for i in range(5):
+        L = [j ^ (j >> i) for j in range(N)]
+        total += sum(L)
+        del L # remove reference to L
+    return total
